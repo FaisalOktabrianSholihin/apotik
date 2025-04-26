5616200bdb5a40bdd991ad5af8544409ba556bba
Faisal Oktabrian Sholihin <faisalokta08@gmail.com>
2025-04-27 06:20:14 +0700

    update penjualan dan pembelian

---
+++ app/Filament/Resources/PembelianResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\Pembelian;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\PembelianResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\PembelianResource\RelationManagers;
+
+class PembelianResource extends Resource
+{
+    protected static ?string $model = Pembelian::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        Select::make('supplier_id')
+                            ->relationship('supplier', 'nama') // Relasi supplier, tampilkan field 'nama'
+                            ->searchable()
+                            ->preload()
+                            ->label('Supplier')
+                            ->required(),
+                        DatePicker::make('tanggal_pembelian')
+                            ->label('Tanggal Pembelian')
+                            ->required(),
+                        TextInput::make('diskon')
+                            ->numeric()
+                            ->label('Diskon')
+                            ->default(0),
+                    ])
+                    ->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('supplier.nama')->label('Nama Supplier')->sortable()->searchable(),
+                TextColumn::make('tanggal_pembelian')->sortable()->searchable(),
+                TextColumn::make('diskon')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPembelians::route('/'),
+            'create' => Pages\CreatePembelian::route('/create'),
+            'edit' => Pages\EditPembelian::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/PenjualanResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\Penjualan;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\DatePicker;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\PenjualanResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\PenjualanResource\RelationManagers;
+
+class PenjualanResource extends Resource
+{
+    protected static ?string $model = Penjualan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        Select::make('pelanggan_id')
+                            ->relationship('pelanggan', 'nama')
+                            ->searchable()
+                            ->preload()
+                            ->label('Pelanggan')
+                            ->required(),
+                        DatePicker::make('tanggal_penjualan')
+                            ->label('Tanggal Penjualan')
+                            ->required(),
+                        TextInput::make('diskon')
+                            ->numeric()
+                            ->label('Diskon')
+                            ->default(0),
+                    ])
+                    ->columns(2),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('pelanggan.nama')->label('Nama Pelanggan')->sortable()->searchable(),
+                TextColumn::make('tanggal_penjualan')->sortable()->searchable(),
+                TextColumn::make('diskon')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPenjualans::route('/'),
+            'create' => Pages\CreatePenjualan::route('/create'),
+            'edit' => Pages\EditPenjualan::route('/{record}/edit'),
+        ];
+    }
+}
